69d6440dc200fb75d9977c359dd72d5fa71c9c6d
caoheyang <328690901@qq.com>
2019-09-29 09:37:34 +0800

    INIT

---
+++ src/main/java/com/example/dockerlogtest/DockerlogtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerlogtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerlogtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerlogtestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/dockerlogtest/LogController.java
@@ -0,0 +1,26 @@
+package com.example.dockerlogtest;
+
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Response;
+
+
+@RestController
+public class LogController {
+    private static final Logger logger = LoggerFactory.getLogger(LogController.class);
+
+    @GetMapping("/info")
+    public ResponseEntity<String> info() {
+        logger.info("试一下log的实时打印");
+        return ResponseEntity.status(HttpStatus.OK).body("ok");
+
+    }
+
+}
